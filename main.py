f025c55dcf8e849e7506fd7ab73fedfe68d0ffd1
Mikunu <nonamebeautifultrap@gmail.com>
2021-11-22 00:42:42 +0600

    Simple channel downloader

---
+++ main.py
@@ -0,0 +1,53 @@
+from pytube import YouTube, Channel, request
+from os import mkdir, path
+from transliterate import translit
+
+
+def create_dir(dirPath):
+    if not path.exists(dirPath):
+        mkdir(dirPath)
+        print("Directory ", dirPath, " Created ")
+    else:
+        print("Directory ", dirPath, " already exists")
+
+
+def download_info(channel):
+    to_download: int = len(channel)
+    total_size: int = 0
+    for video in channel:
+        total_size += request.filesize(video)
+    print(f'{to_download} videos to download. Total size {total_size/1024} megabytes.')
+
+
+def download_video(stream, dirPath=None):
+    if dirPath is None:
+        stream.download()
+    else:
+        stream.download(dirPath)
+
+
+def download_channel(channel, dirPath=None):
+    download_info(channel)
+    if dirPath is not None:
+        dirPath = f'{dirPath}\\{translit(channel.channel_name, language_code="ru", reversed=True)}'
+        print(dirPath)
+    create_dir(dirPath)
+    total_videos = len(channel)
+    iteration = 1
+    for video in channel:
+        video = YouTube(video)
+        print(f'[{iteration}/{total_videos}] {video.title} is downloading...')
+        stream = video.streams.get_by_itag(22)
+        download_video(stream, dirPath)
+        print(f'{video.title} downloaded')
+        iteration += 1
+
+
+def main():
+    channel = Channel('https://www.youtube.com/channel/UCse94379EWfi78PC2w6297w')
+    dirPath = r'E:\YouTube Downloader'
+    download_channel(channel, dirPath)
+
+
+if __name__ == '__main__':
+    main()
